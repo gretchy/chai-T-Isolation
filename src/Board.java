f31606a0c48261c134933ae2c2efbc3d0d3cb123
Gretchen Lai <glai@konicom.com>
2018-05-21 14:06:59 -0700

    small code additions

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 
 public class Board {
 	private String[][] board = new String[8][8];
+	private ArrayList<String> xMoves = new ArrayList<String>();
+	private ArrayList<String> oMoves = new ArrayList<String>();
 	
 	public Board() {
 		initialize();
@@ -20,10 +23,62 @@ public class Board {
 		}
 	}
 	
+	public void makeMove(String player, String move) {
+		int[] playerLoc = getPosition(player);
+		
+		int newRow;
+		if(move.startsWith("A"))
+			newRow = 0;
+		else if(move.startsWith("B"))
+			newRow = 1;
+		else if(move.startsWith("C"))
+			newRow = 2;
+		else if(move.startsWith("D"))
+			newRow = 3;
+		else if(move.startsWith("E"))
+			newRow = 4;
+		else if(move.startsWith("F"))
+			newRow = 5;
+		else if(move.startsWith("G"))
+			newRow = 6;
+		else
+			newRow = 7;
+		
+		int newCol = Integer.parseInt(move.substring(1)) - 1;
+		
+		if(player.equals("X")) {
+			board[newRow][newCol] = "X";
+			xMoves.add(move);
+		}
+		else {
+			board[newRow][newCol] = "O";
+			oMoves.add(move);
+		}
+		
+		board[playerLoc[0]][playerLoc[1]] = "#";
+		
+		printBoard();
+	}
+	
+	public int[] getPosition(String player) {
+		int[] position = new int[2];
+		
+		for(int row = 0; row < board.length; row++) {
+			for(int col = 0; col < board.length; col++) {
+				if(board[row][col].equals(player)) {
+					position[0] = row;
+					position[1] = col;
+				}
+			}
+		}
+		
+		return position;
+	}
+	
 	public void printBoard() {
 		String[] rowIndexes = new String[]{"A", "B", "C", "D", "E", "F", "G", "H"};
 		
-		System.out.println("  1 2 3 4 5 6 7 8");
+		System.out.println("\n  1 2 3 4 5 6 7 8");
 		for(int row = 0; row < board.length; row++) {
 			String completeRow = rowIndexes[row] + " ";
 			for(int col = 0; col < board.length; col++) {
--- src/IsolationMain.java
+++ src/IsolationMain.java
@@ -10,6 +10,28 @@ public class IsolationMain {
 		System.out.println("      CS 420 - Spring 2018");
 		System.out.println("********************************");
 		
-		Board board = new Board();
+		//System.out.print("\nTime Limit per move: ");
+		//int timeLimit = keyboard.nextInt(); // add check to make sure user input is a valid number
+		System.out.print("First player (C or O)? ");
+		String player = keyboard.nextLine(); // add check to make sure user input is valid string
+		
+		if(player.equals("C")) {
+			Board board = new Board();
+			board.makeMove("X", "B2");
+			board.makeMove("O", "G7");
+		}
+		else {
+			
+		}
+		
+		keyboard.close();
+	}
+	
+	public boolean isValidMove(Board board, String player, String move) {
+		boolean isValid = true;
+		
+		
+		
+		return isValid;
 	}
 }
